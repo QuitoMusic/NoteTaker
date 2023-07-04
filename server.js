b5852c404b268b9ca8cf0e9b41594c8889da1605
Francisco Gonzalez <f86gonzalez@outlook.com>
2023-07-04 13:12:12 -0400

    Created routes for html files and api's/ Also UUIDS

--- server.js
+++ server.js
@@ -6,6 +6,52 @@ const fs = require('fs');// reads and writes files
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+let notes = [];
+
+// Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+
+//Creates new id per note saved
+app.get('/api/uuids', (req, res) => {
+  const uuidList = Array.from({ length: 10 }, () => uuidv4());
+  const filePath = path.join(__dirname, 'uuids.txt');
+  const fileData = uuidList.join('\n');
+  
+  fs.writeFile(filePath, fileData, (err) => {
+    if (err) {
+      console.error('Failed to write UUIDs to file:', err);
+      res.status(500).json({ message: 'Failed to generate and download UUIDs' });
+    } else {
+      res.download(filePath, 'uuids.txt', (err) => {
+        if (err) {
+          console.error('Failed to download UUIDs:', err);
+          res.status(500).json({ message: 'Failed to generate and download UUIDs' });
+        }
+        fs.unlink(filePath, (err) => {
+          if (err) {
+            console.error('Failed to delete temporary file:', err);
+          }
+        });
+      });
+    }
+  });
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
